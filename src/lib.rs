e8ab6385d752d56bbb04e6fd91c3429643dc300f
Michael Molinari <michael122096@hotmail.com>
2021-11-28 22:26:23 -0500

    Updated PartialEq, was not comparing ptr before

--- src/lib.rs
+++ src/lib.rs
@@ -132,17 +132,19 @@ impl fmt::Debug for RawParts<T> {
 
 impl<T> PartialEq for RawParts<T> {
     fn eq(&self, other: &Self) -> bool {
-        self.length == other.length  && self.capacity == other.capacity
+        //self.ptr == other.ptr && 
+        self.length == other.length &&
+        self.capacity == other.capacity
     }
 }
-
+/*
 impl Hash for RawParts<T> {
     fn hash<H: Hasher>(&self, state: & mut H) {
         self.length.hash(state);
         self.capacity.hash(state);
     }
 }
-
+*/
 // Do not implement the `From` trait in the other direction since `crate::from`
 // is an unsafe function.
 //
@@ -363,7 +365,7 @@ mod tests {
     fn partial_eq_eq() {
         let mut vec_1 = Vec::with_capacity(100); // capacity is 100
         vec_1.extend_from_slice(b"123456789"); // length is 9
-        let mut vec_2 = Vec::with_capacity(101); // capacity is 100
+        let mut vec_2 = Vec::with_capacity(101); // capacity is 101
         vec_2.extend_from_slice(b"123456789"); // length is 9
 
         let raw_parts_1 = RawParts::from_vec(vec_1);
@@ -373,10 +375,13 @@ mod tests {
 
     #[test]
     fn partial_eq_ne() {
-        let mut vec = Vec::with_capacity(100); // capacity is 100
-        vec.extend_from_slice(b"123456789"); // length is 9
+        let mut vec_1 = Vec::with_capacity(100); // capacity is 100
+        vec_1.extend_from_slice(b"123456789"); // length is 9
+        let mut vec_2 = Vec::with_capacity(101); // capacity is 101
+        vec_2.extend_from_slice(b"123456789"); // length is 9
 
-        let raw_parts_1 = RawParts::from_vec(vec);
+        let raw_parts_1 = RawParts::from_vec(vec_1);
+        let raw_parts_2 = RawParts::from_vec(vec_2);
         assert_ne!(raw_parts_1, raw_parts_2);
     }
 }
